749bfa18b2cd958bb8cdf5859d5f32283707f607
Xize Wu <wuxize1995@gmail.com>
2023-06-13 18:12:15 -0400

    Initial commit

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+export default function AboutPage(){
+  return (
+    <div>
+      <h1>
+      About Page
+      </h1>
+      <p>
+      This is a receipt sharing website built for Frodo Baggins and his friends. We are dedicated to sharing the six-meals-a-day food culture with humans of the Seventh Age.
+      </p>
+    </div>
+  )
+}
---
+++ pages/index.js
@@ -0,0 +1,5 @@
+export default function HomePage(){
+  return <div>
+    <h1>Eat well, my friends!</h1>
+    </div>
+}
